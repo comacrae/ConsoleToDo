f3dbac42ee1363356eb9d815cf0f0671e698d5e7
comacrae <comacrae1995@gmail.com>
2025-02-02 17:36:08 -0600

    created tests and function for ContainsInvalidChars

--- ConsoleToDoProject/ConsoleToDoProject.Tests/ValidatorTests.cs
+++ ConsoleToDoProject/ConsoleToDoProject.Tests/ValidatorTests.cs
@@ -10,7 +10,8 @@ public class ValidatorTests
     public void IsNullString_NullStringEntered_ReturnsTrue()
     {
         Validator validator = new Validator();
-        bool output = validator.IsNullString(null);
+        string input = null;
+        bool output = validator.IsNullString(input);
         Assert.True(output);
     }
 
@@ -27,5 +28,59 @@ public class ValidatorTests
         Validator validator= new Validator();
         bool output = validator.IsNullString("");
         Assert.False(output);
+    }
+
+    [Fact]
+    public void ContainsWhitespace_NullStringEntered_ThrowsNullArgumentException()
+    {
+        Validator validator = new Validator();
+        Assert.Throws<ArgumentNullException>(() => validator.ContainsWhitespace(null));
+    }
+
+    [Fact]
+    public void ContainsWhitespace_EmptyStringEntered_ReturnsFalse()
+    {
+        Validator validator = new Validator();
+        bool result = validator.ContainsWhitespace("");
+        Assert.False(result);
+    }
+
+    [Fact]
+    public void ContainsWhitespace_NoWhitespaceStringEntered_ReturnsFalse()
+    {
+        Validator validator = new Validator();
+        bool result = validator.ContainsWhitespace("thisisatest");
+        Assert.False(result);
+    }
+
+    [Fact]
+    public void ContainsWhitespace_StringWithWhitespace_ReturnsTrue()
+    {
+        Validator validator = new Validator();
+        bool result = validator.ContainsWhitespace("ab c");
+        Assert.True(result);
+    }
+
+    [Fact]
+    public void ContainsInvalidChars_StringWithInvalidChars_ReturnsTrue()
+    {
+        Validator validator = new Validator();
+        bool result = validator.ContainsInvalidChars("ab*(");
+        Assert.True(result);
+    }
+
+    [Fact] 
+    public void ContainsInvalidChars_StringWithValidChars_ReturnsFalse()
+    {
+        Validator validator = new Validator();
+        bool result = validator.ContainsInvalidChars("Test_");
+        Assert.False(result);
+    }
+
+    [Fact]
+    public void ContainsInvalidChars_NullEntered_ThrowsNullArgumentException()
+    {
+        Validator validator = new Validator();
+        Assert.Throws<ArgumentNullException>(() => validator.ContainsInvalidChars(null));
     }
 }
--- ConsoleToDoProject/ConsoleToDoProject/CLI/Validator.cs
+++ ConsoleToDoProject/ConsoleToDoProject/CLI/Validator.cs
@@ -1,9 +1,11 @@
 ﻿using System;
+using System.Text.RegularExpressions;
 
 namespace ConsoleToDoProject.CLI
 {
     public class Validator
     {
+
         public Validator() { }
 
         public bool IsNullString(string? input)
@@ -11,5 +13,37 @@ namespace ConsoleToDoProject.CLI
             return input == null;
         }
 
+        public bool ContainsWhitespace(string? input)
+        {
+            if (IsNullString(input))
+            {
+                throw new ArgumentNullException($"Variable {nameof(input)} cannot be null.");
+            }
+            else
+            {
+
+                string pattern = @"\s+";
+
+                return Regex.IsMatch(input, pattern);
+            }
+
+        }
+
+        public bool ContainsInvalidChars(string? input) { 
+
+            if (IsNullString(input))
+            {
+                throw new ArgumentNullException($"Variable {nameof(input)} cannot be null.");
+            }
+            else
+            {
+
+                string pattern = @"[^a-zA-Z_]+";
+
+                return Regex.IsMatch(input, pattern);
+            }
+        }
+
     }
+
 }
